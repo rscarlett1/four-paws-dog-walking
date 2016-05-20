adbc0037182f50a5266d90640bc47975d1edad23
raewynne scarlett <raewynne.scarlett@gmail.com>
2016-05-20 13:06:25 +1200

    update accordian and map added map not working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,65 @@
+//map code
+var marker;
+var markerdogwalk;
+function initMap(){
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: {lat: -41.132085, lng: 175.029932},
+          mapTypeId: google.maps.MapTypeId.STREET
+        });
+
+        var flightPlanCoordinates = [
+          {lat:  -41.130020, lng:175.031356},
+          {lat: -41.135417, lng:175.034820},
+          {lat: -41.136600, lng: 175.032479},
+          {lat: -41.133292, lng: 175.027011},
+          {lat: -41.130020, lng: 175.027011},
+          {lat:  -41.130020, lng:175.031356},
+
+
+        ];
+        var dogWalk = new google.maps.Polyline({
+          path: flightPlanCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        
+        var myLatLng = {lat: -41.130020, lng: 175.031356};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: myLatLng
+        });
+
+        markerdogwalk = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Dog walk Starts and finishes here!',
+          icon: "http://placehold.it/50x50"
+        });
+
+        
+      markerdogwalk.addListener('click', toggleBounce);
+      dogWalk.setMap(map);
+}
+
+    function toggleBounce() {
+      if (markerdogwalk.getAnimation() !== null) {
+        markerdogwalk.setAnimation(null);
+      } else {
+        markerdogwalk.setAnimation(google.maps.Animation.BOUNCE);
+      }
+        
+}
+
+
+
+
+
+
 $( ".cross" ).hide();
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
@@ -13,3 +75,20 @@ $( ".cross" ).click(function() {
 		$( ".hamburger" ).show();
 	});
 });
+
+//Accordian code
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+
+
+     
+    
+    
+
